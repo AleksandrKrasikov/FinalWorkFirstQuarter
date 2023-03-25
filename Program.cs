72bd1de8bb71584995fd436211f34f1627d3d4dd
Krasikov Aleksandr <3511667@gmail.com>
2023-03-25 21:02:49 +0300

    FinishProgram.cs

--- Program.cs
+++ Program.cs
@@ -7,5 +7,52 @@
 ["Russia", "Denmark", "Kazan"] -> []       */ 
 
 
+// Ввод массива на старте выполнения.
 
+//string[] input = new string[4] {"hello", "2", "world", ":)"};
+
+//string[] input = new string[4] {"1234", "1567", "-2", "computer science"};
+
+//string[] input = new string[3] {"Russia", "Denmark", "Kazan"};
+
+
+
+// Запросить у пользователя ввод массива строк.
+
+Console.WriteLine("Введите строки через пробел: ");       
+
+string[] input = Console.ReadLine()!.Split(' ');
+
+// Создать новый массив строк, содержащий только строки, длина которых меньше или равна 3-ем символам/
+
+string[] output = new string[input.Length];
+
+int count = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if(input[i].Length <= 3)
+    {
+        output[count] = input[i];
+        count++;
+    }
+}
+
+Array.Resize(ref output, count);
+
+// Вывести результат.
+
+Console.WriteLine("Результат: ");
+
+Console.WriteLine();
+
+Console.WriteLine($"[{string.Join(", ", input)}] -> [{string.Join(", ", output)}]");
+
+Console.WriteLine();
+
+// Завершить программу/
+
+Console.WriteLine("Нажмите любую клавишу для завершения программы.");
+
+Console.ReadLine();
 
